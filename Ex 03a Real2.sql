a52a7ad462ba1b25ead5fa74f4fb5c90c0eb5025
NikoFalco <55203485+NikoFalco@users.noreply.github.com>
2019-09-27 13:46:17 -0500

    Delete Ex 03a Real2.sql

--- Ex 03a Real2.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `department` (
-  `DepartmentName` char(35) NOT NULL,
-  `BudgetCode` char(30) NOT NULL,
-  `OfficeNumber` char(15) NOT NULL,
-  `DepartmentPhone` char(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
