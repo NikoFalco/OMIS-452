1599c982c3e5ada33a554e0ec1ecb79a59e63a0d
NikoFalco <55203485+NikoFalco@users.noreply.github.com>
2019-09-27 13:46:03 -0500

    Delete Ex 03a Real.sql

--- Ex 03a Real.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `assignment` (
-  `ProjectID` int(11) NOT NULL,
-  `EmployeeNumber` int(11) NOT NULL,
-  `HoursWorked` decimal(6,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
