70ea15729a7dc2a048197d05285616c527221a89
NikoFalco <55203485+NikoFalco@users.noreply.github.com>
2019-10-07 12:13:55 -0500

    Create Query 14.sql

---
+++ Ex 04a/Query 14.sql
@@ -0,0 +1,5 @@
+/* *** SQL-Query-CH03-14 Niko and Kenny *** */
+SELECT FirstName, LastName, Department, OfficePhone
+FROM EMPLOYEE
+WHERE Department = 'Accounting'
+AND OfficePhone = '360-285-8430';
